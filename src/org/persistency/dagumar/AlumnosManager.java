34cde4b423cf49a9e16ec475ae4477a5086bde60
David Gutiérrez <dagumar@gmail.com>
2012-04-19 13:17:11 +0200

    Refactoring the code

--- src/org/persistency/dagumar/AlumnosManager.java
+++ src/org/persistency/dagumar/AlumnosManager.java
@@ -9,43 +9,54 @@ public class AlumnosManager {
 	private String surname = "";
 	private Integer mark = null;
 	public static final String TABLE = "alumnos";
-	public static final String KEY_SURNAME  = "surname";
-	public static final String KEY_MARK  = "mark";
-	public static final String DATABASE_CREATE = "" +
-			"CREATE TABLE "+ TABLE +" (" +
-			"	"+ KEY_SURNAME +" varchar2(20), " +
-			"	"+ KEY_MARK +" int(2) " +
-			")";
-	
+	public static final String KEY_SURNAME = "surname";
+	public static final String KEY_MARK = "mark";
+	public static final String DATABASE_CREATE = "" + "CREATE TABLE " + TABLE
+			+ " (" + "	" + KEY_SURNAME + " varchar2(20), " + "	" + KEY_MARK
+			+ " int(2) " + ")";
+
 	private DatabaseHelper dbHelper = null;
 	private SQLiteDatabase db = null;
-	
-	public static AlumnosManager Factory(DatabaseHelper dbHelper){
+
+	public static AlumnosManager Factory(DatabaseHelper dbHelper) {
 		return new AlumnosManager(dbHelper);
 	}
-	
-	private AlumnosManager(DatabaseHelper dbHelper){
+
+	private AlumnosManager(DatabaseHelper dbHelper) {
 		this.dbHelper = dbHelper;
-		
+
+	}
+
+	private AlumnosManager alumnoFromCursor(Cursor cursor, AlumnosManager alumno){
+		alumno.setSurname(cursor.getString(cursor.getColumnIndex(KEY_SURNAME)));
+		alumno.setMark(cursor.getInt(cursor.getColumnIndex(KEY_MARK)));
+		return alumno;
+	}
+	private AlumnosManager alumnoFromCursor(Cursor cursor){
+		this.setSurname(cursor.getString(cursor.getColumnIndex(KEY_SURNAME)));
+		this.setMark(cursor.getInt(cursor.getColumnIndex(KEY_MARK)));
+		return this;
 	}
 	
-	public void getAlumnos(){
+	public void getAlumnos() {
 		SQLiteDatabase db = dbHelper.getWritableDatabase();
 		ContentValues content = new ContentValues();
 		content.put("surname", surname);
 		content.put("mark", mark);
-		Cursor cursor = db.query(TABLE, new String[]{KEY_SURNAME, KEY_MARK}, null, null, null, null, KEY_SURNAME);
-		
-		if(cursor != null){
+		Cursor cursor = db.query(TABLE, new String[] { KEY_SURNAME, KEY_MARK },
+				null, null, null, null, null);
+
+		if (cursor != null) {
 			cursor.moveToFirst();
-			surname = cursor.getString(cursor.getColumnIndex(KEY_SURNAME));
-			mark = cursor.getInt(cursor.getColumnIndex(KEY_MARK));
+			do {
+				alumnoFromCursor(cursor);
+			} while (cursor.moveToNext());
 			cursor.close();
 		}
-		
+
 	}
-	
-	public void createAlumno(String surname, int mark){
+
+	public void createAlumno(String surname, int mark) {
 		SQLiteDatabase db = dbHelper.getWritableDatabase();
 		ContentValues content = new ContentValues();
 		content.put("surname", surname);
@@ -68,5 +79,5 @@ public class AlumnosManager {
 	public void setMark(Integer mark) {
 		this.mark = mark;
 	}
-	
+
 }
